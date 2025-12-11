181e87fb6569ef822a2fa853daf5f3d20971dba7
AlaiWebDev <alainwebdev@free.fr>
2025-12-11 15:35:47 +0100

    END ajout datas + divers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./pages/home";
-import ("./app.css");
+import Home from "./pages/Home";
+// import ("./app.css");
 
 function App() {
   return (
---
+++ src/services/DatasService.jsx
@@ -0,0 +1,9 @@
+import projectsDatas from '../assets/datas/projects.json';
+
+function DatasService() {
+    return(
+        projectsDatas
+    )
+}
+
+export default DatasService
