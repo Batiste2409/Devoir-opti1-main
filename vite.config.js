9dff91c54c6efde2f009af5d6faac009c57caea1
AlainWebDev <59807603+AlaiWebDev@users.noreply.github.com>
2026-06-15 16:08:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -9,12 +8,3 @@ export default defineConfig({
     tailwindcss(),
   ],
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> e05feae95adc8894708ea4f242fa94729ad37c4f
